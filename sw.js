f7774e7852708d3e80ef81112c0a5cb350fd90fb
Víctor Alonso <works.alonsog@gmail.com>
2022-01-18 20:29:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
-const INMUTABLE_CACHE = 'inmutable-v1';
+const STATIC_CACHE = 'static-v2';
+const DYNAMIC_CACHE = 'dynamic-v2';
+const INMUTABLE_CACHE = 'inmutable-v2';
 
 const APP_SHELL = [
     //'/',
